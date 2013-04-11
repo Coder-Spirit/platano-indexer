39e822b99633e0baf42e7645a9ac3f73a1fdc784
Andreu Correa Casablanca <ac@bananity.com>
2013-04-11 12:34:38 +0200

    Added test to ResultItemComparator (at this moment, the test fails)

---
+++ src/test/java/com/bananity/util/ResultItemComparatorTest.java
@@ -0,0 +1,40 @@
+package com.bananity.util;
+
+
+// Main Class
+import com.bananity.util.ResultItemComparator;
+import com.bananity.util.SearchesTokenizer;
+
+// Java Utils
+import java.util.ArrayList;
+
+// Junit
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+/**
+ * @author Andreu Correa Casablanca
+ */
+@RunWith(JUnit4.class)
+public class ResultItemComparatorTest
+{
+	@Test
+	public void test_compare () {
+		ResultItemComparator c1 = new ResultItemComparator(SearchesTokenizer.getSubTokensBag("Bananity"));
+		ResultItemComparator c2 = new ResultItemComparator(SearchesTokenizer.getSubTokensBag("bananity"));
+
+		// Equality comparisons
+		Assert.assertEquals(0, c1.compare("Bananity", "Bananity"));
+		Assert.assertEquals(0, c1.compare("Xananity", "Xananity"));
+		Assert.assertEquals(0, c2.compare("Bananity", "Bananity"));
+		Assert.assertEquals(0, c2.compare("Xananity", "Xananity"));
+
+		// More Interesting comparisons
+		Assert.assertEquals(1, c1.compare("Bananity On", "Bananity"));
+		Assert.assertEquals(-1, c1.compare("Bananity", "Bananity On"));
+	}
+}

907dde15037a68f8b13555f30d12dca3f95d36e0
Andreu Correa Casablanca <ac@bananity.com>
2013-04-16 15:37:15 +0200

    Finished LocksBean implementation (but not its documentation)

--- src/main/java/com/bananity/locks/LocksBean.java
+++ src/main/java/com/bananity/locks/LocksBean.java
@@ -1,14 +1,19 @@
 package com.bananity.locks;
 
+
+// Java Utils
+import java.util.HashMap;
+
+// Java Concurrency Management
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+
 // Bean Setup
 import javax.ejb.EJB;
 import javax.ejb.Startup;
 import javax.ejb.Singleton;
 import javax.annotation.PostConstruct;
-
-// Java Concurrency Management
-import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
 // JBoss Concurrency Management
 import javax.ejb.ConcurrencyManagement;
@@ -21,35 +26,98 @@ import org.apache.log4j.PropertyConfigurator;
 
 
 /**
- *
+ * @author Andreu Correa Casablanca
  */
 @Startup
 @Singleton
 @ConcurrencyManagement(ConcurrencyManagementType.BEAN)
 public class LocksBean {
 
+	/**
+	 * Log4j reference
+	 */
 	private static Logger log;
 
+	/**
+	 * Internal lock, used to update collection and token's locks
+	 */
+	private ReentrantReadWriteLock 	internalLock 		= null;
+	private ReadLock 				internalReadLock 	= null;
+	private WriteLock 				internalWriteLock 	= null;
+
+	/**
+	 * Locks over Collections
+	 */
+	private HashMap<String, ReentrantReadWriteLock> collectionLocks = null;
+
+	/**
+	 *
+	 */
 	@PostConstruct
 	void init() {
 		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 		PropertyConfigurator.configure(classLoader.getResource("log4j.properties"));
 		log = Logger.getLogger(LocksBean.class);
+
+		internalLock 		= new ReentrantReadWriteLock();
+		internalReadLock 	= internalLock.readLock();
+		internalWriteLock 	 = internalLock.writeLock();
+
+		collectionLocks = new HashMap<String, ReentrantReadWriteLock>();
 	}
 
-	ReadLock getCollectionReadLock (String collName) {
-		return null;
+	/**
+	 *
+	 */
+	private ReentrantReadWriteLock getCollectionReeentrantLock (String collName) {
+		internalReadLock.lock();
+		ReentrantReadWriteLock collectionLock = collectionLocks.get(collName);
+
+		if (collectionLock == null) {
+			internalReadLock.unlock();
+			internalWriteLock.lock();
+			
+			collectionLock = collectionLocks.get(collName);
+			if (collectionLock == null) {
+				collectionLock = new ReentrantReadWriteLock();
+				collectionLocks.put(collName, collectionLock);
+			}
+
+			internalWriteLock.unlock();
+		} else {
+			internalReadLock.unlock();
+		}
+
+		return collectionLock;
 	}
 
-	WriteLock getCollectionWriteLock (String collName) {
-		return null;
+	/**
+	 *
+	 */
+	public ReadLock getCollectionReadLock (String collName) {
+		return getCollectionReeentrantLock(collName).readLock();
 	}
 
-	ReadLock getTokenReadLock (String collName, String token) {
-		return null;
+	/**
+	 *
+	 */
+	public WriteLock getCollectionWriteLock (String collName) {
+		return getCollectionReeentrantLock(collName).writeLock();
 	}
 
-	WriteLock getTokenWriteLock (String collName, String token) {
-		return null;
+	/**
+	 *
+	 */
+	public ReadLock getTokenReadLock (String collName, String token) {
+		// TODO : Implement a real token block
+		return getCollectionReadLock(collName);
+	}
+
+	/**
+	 *
+	 */
+	public WriteLock getTokenWriteLock (String collName, String token) {
+		// TODO : Implement a real token block
+		return getCollectionWriteLock(collName);
 	}
 }

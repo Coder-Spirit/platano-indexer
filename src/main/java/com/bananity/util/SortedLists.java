b40f883f2c83681df013fbbf2b2589788d2d8190
Andreu Correa Casablanca <ac@bananity.com>
2013-05-02 14:40:35 +0200

    Added optimization to SortedLists.sortedInsert

--- src/main/java/com/bananity/util/SortedLists.java
+++ src/main/java/com/bananity/util/SortedLists.java
@@ -1,6 +1,7 @@
 package com.bananity.util;
 
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.ArrayList;
 
@@ -9,17 +10,25 @@ import java.util.ArrayList;
  */
 public class SortedLists
 {
+	/**
+	 *
+	 */
 	public static <T> boolean sortedInsert (final Comparator<T> c, final ArrayList<T> al, final int limit, T item) {
 		
 		int oldSize = al.size();
 		int insertPos = oldSize;
 
-		for (int i = al.size()-1; i >= 0; i--) {
-			if (c.compare(al.get(i), item) > 0) {
-				insertPos--;
-			} else {
-				break;
+		// We use a threshold to select between binary search and naive search
+		if (al.size() < 10) {
+			for (int i = al.size()-1; i >= 0; i--) {
+				if (c.compare(al.get(i), item) > 0) {
+					insertPos--;
+				} else {
+					break;
+				}
 			}
+		} else {
+			insertPos = - Collections.binarySearch(al, item, c) - 1;
 		}
 
 		if (insertPos < limit) {
